1412f0dda0e8c97c6ee268674cfe53affe716166
Parksungje <Parksungje0213@gmail.com>
2025-09-23 19:59:29 +0900

    feat : NPC System

--- Assets/PSJ/01.Script/Dialogue/DialogueManager.cs
+++ Assets/PSJ/01.Script/Dialogue/DialogueManager.cs
@@ -15,8 +15,10 @@ namespace PSJ._01.Script.Dialogue
         public TextMeshProUGUI npcNameVal;
         public TextMeshProUGUI contentsVal;
 
-
         private Queue<string> _contentsQueue;
+        private Coroutine typingCoroutine;
+        private bool isTyping = false;
+        private string currentContent = "";
 
         public event Action OnDialogueStart;
         public event Action OnDialogueEnd;
@@ -27,23 +29,45 @@ namespace PSJ._01.Script.Dialogue
             {
                 instance = this;
             }
-            _contentsQueue = new Queue<string>(); // 초기화 추가
+            _contentsQueue = new Queue<string>();
             _contentsQueue.Clear();
         }
 
-        public void DialogueStart(Dialogue dialogue)
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (isTyping)
+                {
+                    // 타이핑 중이면 즉시 전체 문장 출력
+                    if (typingCoroutine != null)
+                        StopCoroutine(typingCoroutine);
+                    contentsVal.text = currentContent;
+                    isTyping = false;
+                }
+                else
+                {
+                    // 다음 대사 출력
+                    GetNextContent();
+                }
+            }
+        }
+
+        public void DialogueStart(string npcName, string[] contents)
         {
             Debug.Log("DialogueStart");
             OnDialogueStart?.Invoke();
-            npcNameVal.text = dialogue.npcName;
+            npcNameVal.text = npcName;
 
-            foreach (string contents in dialogue.contents)
+            _contentsQueue.Clear();
+            foreach (string content in contents)
             {
-                _contentsQueue.Enqueue(contents);   
+                _contentsQueue.Enqueue(content);
             }
 
             GetNextContent();
         }
+
 
         private void GetNextContent()
         {
@@ -53,13 +77,15 @@ namespace PSJ._01.Script.Dialogue
                 return;
             }
             
-            string content = _contentsQueue.Dequeue();
-            StopAllCoroutines();
-            StartCoroutine(ContentsType(content));
+            currentContent = _contentsQueue.Dequeue();
+            if (typingCoroutine != null)
+                StopCoroutine(typingCoroutine);
+            typingCoroutine = StartCoroutine(ContentsType(currentContent));
         }
 
         private IEnumerator ContentsType(string contents)
         {
+            isTyping = true;
             contentsVal.text = string.Empty;
             yield return new WaitForSeconds(0.25f);
             foreach (char letter in contents)
@@ -67,6 +93,7 @@ namespace PSJ._01.Script.Dialogue
                 contentsVal.text += letter;
                 yield return null;
             }
+            isTyping = false;
         }
 
         private void DialogueEnd()
--- Assets/PSJ/01.Script/Dialogue/DialogueNPC.cs
+++ Assets/PSJ/01.Script/Dialogue/DialogueNPC.cs
@@ -1,38 +1,44 @@
 ﻿using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace PSJ._01.Script.Dialogue
 {
     public class DialogueNpc : MonoBehaviour
     {
-        [SerializeField] private Dialogue dialogue;
-        public bool isDialogue = false; 
+        [SerializeField] private string npcName;
+        [TextArea(3, 10)]
+        [SerializeField] private string[] contents;
+
+        public bool isDialogue = false;
 
         private void Start()
         {
             DialogueManager.Instance.OnDialogueEnd += OnDialogueEnd;
-            isDialogue = true;
-            DialogueManager.Instance.DialogueStart(dialogue);
-            if (isDialogue)
-            {
-                Debug.Log("보이루");
-            }
+            StartDialogue();
         }
 
         private void Update()
         {
-            Debug.Log($"isDialogue 상태: {isDialogue}");
+            if (!isDialogue && Input.GetKeyDown(KeyCode.Space))
+            {
+                StartDialogue();
+            }
+        }
+
+        private void StartDialogue()
+        {
+            isDialogue = true;
+            DialogueManager.Instance.DialogueStart(npcName, contents);
         }
 
         private void StopInteract()
         {
             isDialogue = false;
         }
+
         private void OnDialogueEnd()
         {
             StopInteract();
         }
-
     }
 }

a04d32373e7c306729da7cf2adbfc000ced3b96a
Parksungje <Parksungje0213@gmail.com>
2025-09-24 19:51:11 +0900

    fix : Dialogue System

--- Assets/PSJ/01.Script/Dialogue/DialogueNPC.cs
+++ Assets/PSJ/01.Script/Dialogue/DialogueNPC.cs
@@ -9,36 +9,19 @@ namespace PSJ._01.Script.Dialogue
         [TextArea(3, 10)]
         [SerializeField] private string[] contents;
 
-        public bool isDialogue = false;
+        public bool IsDialogue { get; private set; } = false;
+
+        public string NpcName => npcName;
+        public string[] Contents => contents;
 
         private void Start()
         {
             DialogueManager.Instance.OnDialogueEnd += OnDialogueEnd;
-            StartDialogue();
-        }
-
-        private void Update()
-        {
-            if (!isDialogue && Input.GetKeyDown(KeyCode.Space))
-            {
-                StartDialogue();
-            }
-        }
-
-        private void StartDialogue()
-        {
-            isDialogue = true;
-            DialogueManager.Instance.DialogueStart(npcName, contents);
-        }
-
-        private void StopInteract()
-        {
-            isDialogue = false;
         }
 
         private void OnDialogueEnd()
         {
-            StopInteract();
+            IsDialogue = false;
         }
     }
 }
---
+++ Assets/PSJ/01.Script/Dialogue/DialogueUIController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace PSJ._01.Script.Dialogue
+{
+    public class DialogueUIController : MonoBehaviour
+    {
+        public void ShowDialogue(DialogueNpc targetNpc)
+        {
+            if (targetNpc == null) return;
+
+            if (!targetNpc.IsDialogue)
+            {
+                targetNpc.GetType()
+                    .GetProperty("IsDialogue")
+                    ?.SetValue(targetNpc, true, null);
+                DialogueManager.Instance.DialogueStart(
+                    targetNpc.NpcName,
+                    targetNpc.Contents
+                );
+            }
+        }
+    }
+}
